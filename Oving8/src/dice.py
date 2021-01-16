191591c5f287a29d3d8701dbb87d380c7421deeb
Helene Yuee Jonson <55952683+helenejonson@users.noreply.github.com>
2021-01-16 14:43:10 +0100

    Update dice.py

--- Oving8/src/dice.py
+++ Oving8/src/dice.py
@@ -1,127 +1,106 @@
+import random as r
 import turtle
-import random
 
 
-class d6:
-
+class Die:
     def __init__(self, x, y, size):
         self.x = x
         self.y = y
         self.size = size
-        self.pen = turtle.Turtle()
-        self.value = 0
 
     def draw(self):
-        w = turtle.Screen()
-        self.getValue()
-        self.pen.speed(0)
-        self.pen.penup()
-        self.drawDice()
-        self.drawNumber()
-        self.pen.goto(0, 0)
-        w.exitonclick()
+        pen = turtle.Turtle()
+        win = turtle.Screen()
+        self.drawSquare(pen)
+        rand = r.randint(1, 6)
+        radii = (self.size / 10) * 0.75
+        if rand == 1:
+            self.drawOne(pen, radii)
+        elif rand == 2:
+            self.drawTwo(pen, radii)
+        elif rand == 3:
+            self.drawOne(pen, radii)
+            self.drawTwo(pen, radii)
+        elif rand == 4:
+            self.drawFour(pen, radii)
+        elif rand == 5:
+            self.drawFour(pen, radii)
+            self.drawOne(pen, radii)
+        else:
+            self.drawFour(pen, radii)
+            self.drawSix(pen, radii)
+        win.exitonclick()
 
-    def getValue(self):
-        self.value = random.randint(1, 6)
+    def drawSquare(self, pen):
+        pen.up()
+        pen.goto(self.x, self.y)
+        pen.down()
+        pen.fillcolor("blue")
+        pen.begin_fill()
+        for x in range(4):
+            pen.forward(self.size)
+            pen.left(90)
+        pen.end_fill()
 
-    def drawDice(self):
-        self.pen.fillcolor("red")
-        self.pen.pencolor("red")
-        self.pen.goto(self.x, self.y)
-        self.pen.pendown()
-        self.pen.begin_fill()
-        self.pen.forward(self.size)
-        self.pen.right(90)
-        self.pen.forward(self.size)
-        self.pen.right(90)
-        self.pen.forward(self.size)
-        self.pen.right(90)
-        self.pen.forward(self.size)
-        self.pen.left(90)
-        self.pen.end_fill()
-        self.pen.penup()
+    def drawOne(self, pen, radii):
+        pen.up()
+        pen.goto(self.x + (self.size / 2), self.y + (self.size / 2) - radii)
+        pen.down()
+        pen.fillcolor("black")
+        pen.begin_fill()
+        pen.circle(radii)
+        pen.end_fill()
 
-    def drawNumber(self):
-        self.pen.fillcolor("yellow")
-        self.pen.pencolor("yellow")
-        radii = self.size / 20
-        centerx = self.x + self.size / 2
-        centery = self.y - self.size / 2
-        oneThirdX = self.x + self.size / 3
-        oneThirdY = self.y - self.size / 3
-        twoThirdX = self.x + self.size - (self.size / 3)
-        twoThirdY = self.y - self.size + (self.size / 3)
-        if self.value == 1:
-            self.one(centerx, centery, radii)
-        elif self.value == 2:
-            self.two(oneThirdX, oneThirdY, twoThirdX, twoThirdY, radii)
-        elif self.value == 3:
-            self.one(centerx, centery, radii)
-            self.two(oneThirdX, oneThirdY, twoThirdX, twoThirdY, radii)
-        elif self.value == 4:
-            self.four(oneThirdX, oneThirdY, twoThirdX, twoThirdY, radii)
-        elif self.value == 5:
-            self.four(oneThirdX, oneThirdY, twoThirdX, twoThirdY, radii)
-            self.one(centerx, centery, radii)
-        elif self.value == 6:
-            self.four(oneThirdX, oneThirdY, twoThirdX, twoThirdY, radii)
-            self.six(centery, oneThirdX, twoThirdX, radii)
+    def drawTwo(self, pen, radii):
+        pen.up()
+        first = self.size / 6
+        second = 5 * (self.size / 6)
+        for x in range(2):
+            if x == 0:
+                pen.goto(self.x + first, (self.y + second) - radii)
+            else:
+                pen.goto(self.x + second, (self.y + first) - radii)
+            pen.down()
+            pen.fillcolor("black")
+            pen.begin_fill()
+            pen.circle(radii)
+            pen.end_fill()
+            pen.up()
 
-    def one(self, centerx, centery, radii):
-        self.pen.goto(centerx, centery + radii)
-        self.pen.pendown()
-        self.pen.begin_fill()
-        self.pen.circle(radii)
-        self.pen.end_fill()
-        self.pen.penup()
+    def drawFour(self, pen, radii):
+        pen.up()
+        self.drawTwo(pen, radii)
+        first = self.size / 6
+        second = 5 * (self.size / 6)
+        for x in range(2):
+            if x == 0:
+                pen.goto(self.x + first, (self.y + first) - radii)
+            else:
+                pen.goto(self.x + second, (self.y + second) - radii)
+            pen.down()
+            pen.fillcolor("black")
+            pen.begin_fill()
+            pen.circle(radii)
+            pen.end_fill()
+            pen.up()
 
-    def two(self, oneThirdX, oneThirdY, twoThirdX, twoThirdY, radii):
-        self.pen.goto(oneThirdX, oneThirdY + radii)
-        self.pen.pendown()
-        self.pen.begin_fill()
-        self.pen.circle(radii)
-        self.pen.end_fill()
-        self.pen.penup()
-        self.pen.goto(twoThirdX, twoThirdY + radii)
-        self.pen.pendown()
-        self.pen.begin_fill()
-        self.pen.circle(radii)
-        self.pen.end_fill()
-        self.pen.penup()
-
-    def four(self, oneThirdX, oneThirdY, twoThirdX, twoThirdY, radii):
-        self.two(oneThirdX, oneThirdY, twoThirdX, twoThirdY, radii)
-        self.pen.goto(oneThirdX, twoThirdY + radii)
-        self.pen.pendown()
-        self.pen.begin_fill()
-        self.pen.circle(radii)
-        self.pen.end_fill()
-        self.pen.penup()
-        self.pen.goto(twoThirdX, oneThirdY + radii)
-        self.pen.pendown()
-        self.pen.begin_fill()
-        self.pen.circle(radii)
-        self.pen.end_fill()
-        self.pen.penup()
-
-    def six(self, centery, oneThirdX, twoThirdX, radii):
-        self.pen.goto(oneThirdX, centery + radii)
-        self.pen.pendown()
-        self.pen.begin_fill()
-        self.pen.circle(radii)
-        self.pen.end_fill()
-        self.pen.penup()
-        self.pen.goto(twoThirdX, centery + radii)
-        self.pen.begin_fill()
-        self.pen.circle(radii)
-        self.pen.end_fill()
-        self.pen.penup()
+    def drawSix(self, pen, radii):
+        pen.up()
+        first = self.size / 6
+        second = 5 * (self.size / 6)
+        for x in range(2):
+            if x == 0:
+                pen.goto(self.x + first, (self.y + (self.size / 2)) - radii)
+            else:
+                pen.goto(self.x + second, (self.y + (self.size / 2)) - radii)
+            pen.down()
+            pen.fillcolor("black")
+            pen.begin_fill()
+            pen.circle(radii)
+            pen.end_fill()
+            pen.up()
 
 
-if __name__ == "__main__":
-    x = input("X position: ")
-    y = input("y position: ")
-    size = input("Size of dice: ")
-    roll = d6(int(x), int(y), int(size))
-    # roll = d6(50, 50, 100)
-    roll.draw()
+if __name__ == '__main__':
+    a = Die(0, -100, 300)
+    a.draw()
